45cc2a4b6499b1ecf9a2528995ede58490ba0758
Devendra Milmile <d.h.milmile1@gmail.com>
2023-11-13 15:58:08 +0530

    Added Rate limiting

--- RateLimiting/Controllers/WeatherForecastController.cs
+++ RateLimiting/Controllers/WeatherForecastController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.RateLimiting;
 
 namespace RateLimiting.Controllers
 {
@@ -8,8 +9,8 @@ namespace RateLimiting.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-    };
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
 
         private readonly ILogger<WeatherForecastController> _logger;
 
@@ -18,8 +19,60 @@ namespace RateLimiting.Controllers
             _logger = logger;
         }
 
-        [HttpGet(Name = "GetWeatherForecast")]
-        public IEnumerable<WeatherForecast> Get()
+        [HttpGet]
+        [Route("GetWeatherForecastToken")]
+        [EnableRateLimiting("TokenBucketPolicy")]
+        [ProducesResponseType(typeof(IEnumerable<WeatherForecast>), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status429TooManyRequests)]
+        public IEnumerable<WeatherForecast> GetToken()
+        {
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+
+        [HttpGet]
+        [Route("GetWeatherForecastSliding")]
+        [EnableRateLimiting("SlidingWindowPolicy")]
+        [ProducesResponseType(typeof(IEnumerable<WeatherForecast>), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status429TooManyRequests)]
+        public IEnumerable<WeatherForecast> GetSliding()
+        {
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+
+        [HttpGet]
+        [Route("GetWeatherForecastFixed")]
+        [EnableRateLimiting("FixedWindowPolicy")]
+        [ProducesResponseType(typeof(IEnumerable<WeatherForecast>), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status429TooManyRequests)]
+        public IEnumerable<WeatherForecast> GetFixed()
+        {
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+
+        [HttpGet]
+        [Route("GetWeatherForecastConcurrent")]
+        [EnableRateLimiting("ConcurrentPolicy")]
+        [ProducesResponseType(typeof(IEnumerable<WeatherForecast>), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status429TooManyRequests)]
+        public IEnumerable<WeatherForecast> GetConcurrent()
         {
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
--- RateLimiting/Program.cs
+++ RateLimiting/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.AspNetCore.RateLimiting;
+using System.Threading.RateLimiting;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,61 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// configure rate limiting
+builder.Services.AddRateLimiter(options =>
+{
+    options.RejectionStatusCode = StatusCodes.Status429TooManyRequests;
+    /*
+     Take 10 request if exceed in one hour then it throw 429
+     After every 1 hour 1 request restore and we can consume it
+     */
+    options.AddTokenBucketLimiter("TokenBucketPolicy",opt =>
+    {
+        opt.TokenLimit = 10;
+        opt.ReplenishmentPeriod = TimeSpan.FromHours(1);
+        opt.TokensPerPeriod = 1;
+        opt.AutoReplenishment = true;
+    });
+
+    /*
+        Each time window is divided into multiple segments
+        The window slides one segment each segment interval
+        The segment interval is (window_time)/(segments_per_window)
+        When a segment expires, the requests taken in that segment are added to the current segment
+     */
+    options.AddSlidingWindowLimiter("SlidingWindowPolicy", opt =>
+    {
+        opt.QueueLimit = 0;
+        opt.PermitLimit = 10;
+        opt.AutoReplenishment = true;
+        opt.QueueProcessingOrder = QueueProcessingOrder.OldestFirst;
+        opt.Window = TimeSpan.FromSeconds(10);
+        opt.SegmentsPerWindow = 2;
+    });
+
+    /*
+        The Window value determines the time window.
+     */
+    options.AddFixedWindowLimiter("FixedWindowPolicy", opt =>
+    {
+        opt.QueueLimit = 0;
+        opt.PermitLimit = 10;
+        opt.AutoReplenishment = true;
+        opt.QueueProcessingOrder = QueueProcessingOrder.OldestFirst;
+        opt.Window = TimeSpan.FromSeconds(10);
+    });
+
+    /*
+        The concurrency limiter is the most straightforward algorithm, and it just limits the number of concurrent requests.
+     */
+    options.AddConcurrencyLimiter("ConcurrentPolicy", opt =>
+    {
+        opt.QueueLimit = 0;
+        opt.PermitLimit = 1;
+        opt.QueueProcessingOrder = QueueProcessingOrder.OldestFirst;
+    });
+});
 
 var app = builder.Build();
 
@@ -17,6 +75,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseAuthorization();
+
+app.UseRateLimiter();
 
 app.MapControllers();
 
